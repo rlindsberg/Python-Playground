6c9973046bb620ca75c74eb45acf448d7039a395
rlindsberg <roderick@karlemstrand.com>
2019-11-04 17:54:21 +0100

    Save vacancies to SQLite

--- scrape.py
+++ scrape.py
@@ -8,9 +8,17 @@ Copyright (c) waved, Roderick Karlemstrand - No Rights Reserved
 """
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import sqlite3
 
 
 def main():
+    # create database
+    conn = sqlite3.connect('scraped_vacancies.sqlite')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE Vacancies (Vacancy_title VARCHAR, Company_name VARCHAR, Location VARCHAR)')
+    conn.commit()
+
+    # prepare for BeautifulSoup
     quote_page = 'https://www.stepstone.se/lediga-jobb-i-hela-sverige/data-it/'
     page = urlopen(quote_page)
     soup = BeautifulSoup(page, 'html.parser')
@@ -25,6 +33,13 @@ def main():
         job_location = job.find('span', attrs={'class': 'subtitle'}).text.split('·')[1]
         print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Found a new job!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         print(("%s; %s; %s \n\n" % (job_company, job_title, job_location)))
+
+        # save job to database. SQL-injection is not welcomed
+        cur.execute('INSERT INTO Vacancies (Vacancy_title, Company_name, Location) VALUES (?, ?, ?)',
+                    (job_title, job_company, job_location))
+        conn.commit()
+
+    conn.close()
 
     print('Debugging...')
 
