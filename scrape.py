b5a2a2a9ddcd3f10516bd08d0fd90432a509db8c
rlindsberg <roderick@karlemstrand.com>
2019-11-05 17:52:02 +0100

    Rebase commit #596aa63 on top of master

--- scrape.py
+++ scrape.py
@@ -27,6 +27,7 @@ def main():
     page = urlopen(quote_page)
     soup = BeautifulSoup(page, 'html.parser')
 
+    """ Parse Categories"""
     # parse html
     job_categories = soup.find(id='frontpage-category-list').find_all('a')
     # cat is {Tag}
@@ -36,26 +37,46 @@ def main():
         job_cat = cat_with_no_of_jobs.split('(', 1)[0][0:-1]
         cat_url = cat['href']
 
+        print('Scraping category: ' + job_cat + '\n')
+
         # do old parsing stuff
-        sub_page = urlopen(cat_url)
-        sub_soup = BeautifulSoup(sub_page, 'html.parser')
-        job_list = sub_soup.find_all('article', attrs={'data-id': ''})
+        cat_page = urlopen(cat_url)
+        cat_soup = BeautifulSoup(cat_page, 'html.parser')
 
-        # parse the first job
-        for job in job_list:
-            job_company = job.find('span', attrs={'class': 'text-bold'}).text
-            job_title = job.find('h5').text
-            job_location = job.find('span', attrs={'class': 'subtitle'}).text.split('·')[1]
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Found a new job!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-            print(("%s; %s; %s \n\n" % (job_company, job_title, job_location)))
+        """ Parse Sub-pages"""
+        # parse the nav bar and find the last page
+        page_list = cat_soup.find(id='pagination').find_all('a')
+        last_page = page_list[-1]['href']
+        last_page_number = last_page.split('/')[-2].split('sida')[1]
 
-            # save job to database. SQL-injection is not welcomed
-            cur.execute('INSERT INTO Vacancies (Vacancy_title, Category, Company_name, Location) VALUES (?, ?, ?, ?)',
-                        (job_title, job_cat, job_company, job_location))
-            conn.commit()
-        # sleep for 1 sec so that we don't break the website
-        time.sleep(1)
-    # parsed all cats
+        # iterate every page
+        for page_number in range(1, last_page_number+1):
+
+            print('Scraping page #' + page_number + '\n')
+            
+            page_url = quote_page + 'sida' + str(page_number) + '/'
+            sub_page = urlopen(page_url)
+            sub_soup = BeautifulSoup(sub_page, 'html.parser')
+
+            """ Parse Vacancies"""
+            job_list = sub_soup.find_all('article', attrs={'data-id': ''})
+
+            # parse the first job
+            for job in job_list:
+                job_company = job.find('span', attrs={'class': 'text-bold'}).text
+                job_title = job.find('h5').text
+                job_location = job.find('span', attrs={'class': 'subtitle'}).text.split('·')[1]
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Found a new job!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                print(("%s; %s; %s \n\n" % (job_company, job_title, job_location)))
+
+                # save job to database. SQL-injection is not welcomed
+                cur.execute('INSERT INTO Vacancies (Vacancy_title, Category, Company_name, Location) VALUES (?, ?, ?, ?)',
+                            (job_title, job_cat, job_company, job_location))
+                conn.commit()
+            # sleep for 1 sec so that we don't break the website
+            time.sleep(1)
+
+    # parsed all cats. All done.
     conn.close()
 
     print('Debugging...')
